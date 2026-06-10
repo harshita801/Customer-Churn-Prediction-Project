b90347c9103e160c2fd3f323d1a7da3439b9ffaa
Karra Harshita <karraharshita@gmail.com>
2026-06-10 19:11:55 +0530

    Updated Changes

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+st.write("App started")
+
+import joblib
+
+st.write("Joblib imported")
+
+model = joblib.load("churn_model.pkl")
+
+st.write("Model loaded successfully")
+
 # Load model
 model = joblib.load("churn_model.pkl")
 
